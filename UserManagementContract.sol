cb7be39a429aa1568599754feb713b6e28589da5
NADAVADI HARSHITH GOWD <nadavadiharshith.gowd2020@vitstudent.ac.in>
2023-11-19 19:50:52 +0530

    Update UserManagementContract.sol

--- UserManagementContract.sol
+++ UserManagementContract.sol
@@ -7,3 +7,14 @@ contract UserManagementContract {
         string password; // For simplicity, you might want to use more secure authentication methods in a real-world scenario
         // Add other user details here
     }
+
+    mapping(address => User) public users;
+    mapping(address => bool) public isUserRegistered;
+
+    event UserRegistered(address indexed userAddress, string name);
+    event UserLoggedIn(address indexed userAddress);
+
+    modifier onlyRegisteredUser() {
+        require(isUserRegistered[msg.sender], "User not registered.");
+        _;
+    }
