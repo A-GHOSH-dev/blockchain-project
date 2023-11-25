95f5d7f9ee4ba2b39162cf2b062a5ddee9c32209
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2023-11-25 21:33:37 +0530

    Add files via upload

--- proofva;idation.js
+++ proofva;idation.js
@@ -1,21 +1,11 @@
 // blockchain.js
-
-// Initialize Web3 with your Ethereum node URL
 const web3 = new Web3('YOUR_ETHEREUM_NODE_URL');
-
-// Replace with the ABI (Application Binary Interface) of your smart contract
-const contractABI = [...];
-
-// Replace with the address of your deployed smart contract
-const contractAddress = '0x...';
-
-// Create an instance of the smart contract using Web3
+const contractABI = [...]; // Replace with your ABI
+const contractAddress = '0x...'; // Replace with your contract address
 const carbonCreditContract = new web3.eth.Contract(contractABI, contractAddress);
 
-// Async function to propose proof of carbon reduction for a challenge
 async function proposeProof(challengeId, credits) {
     try {
-        // Get the user's Ethereum accounts
         const accounts = await web3.eth.getAccounts();
         const userAddress = accounts[0];
 
@@ -23,24 +13,19 @@ async function proposeProof(challengeId, credits) {
         await carbonCreditContract.methods.proposeCarbonReductionProof(challengeId, credits)
             .send({ from: userAddress });
 
-        // Display a success message
         alert('Proof submitted successfully!');
     } catch (error) {
-        // Log an error if submission fails
         console.error('Error submitting proof:', error);
     }
 }
 
-// Async function to validate proof of carbon reduction for a challenge
 async function validateProof(challengeId, userAddress) {
     try {
         // Call the smart contract method to get user credits for a challenge
         const credits = await carbonCreditContract.methods.getUserCreditsForChallenge(userAddress, challengeId).call();
 
-        // Display the user's credits for the challenge
         alert(`User credits for Challenge ${challengeId}: ${credits}`);
     } catch (error) {
-        // Log an error if validation fails
         console.error('Error validating proof:', error);
     }
 }
