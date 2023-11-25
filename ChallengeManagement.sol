9b50a9a84429f8942c9303c0f7937f0ec65aa053
NADAVADI HARSHITH GOWD <nadavadiharshith.gowd2020@vitstudent.ac.in>
2023-11-25 20:17:18 +0530

    Update ChallengeManagement.sol

--- ChallengeManagement.sol
+++ ChallengeManagement.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
+// Specify the license for the smart contract
+
 pragma solidity ^0.8.0;
+// Declare the version of Solidity the smart contract is written in
 
 contract ChallengeManagement {
+    // Define a struct representing a Challenge with various attributes
     struct Challenge {
         uint256 challengeId;
         string name;
@@ -11,9 +15,11 @@ contract ChallengeManagement {
         uint256 mediumLevelTarget;
         uint256 hardLevelTarget;
     }
-    
+
+    // Declare a mapping to store Challenge structs with their unique challengeId as the key
     mapping(uint256 => Challenge) public challenges;
-    
+
+    // Function to create a new challenge with specified parameters
     function createChallenge(
         string memory _name,
         uint256 _startTime,
@@ -22,8 +28,10 @@ contract ChallengeManagement {
         uint256 _mediumLevelTarget,
         uint256 _hardLevelTarget
     ) public {
-        // Create a new challenge
+        // Generate a unique challengeId using the hash of relevant parameters and the sender's address
         uint256 challengeId = uint256(keccak256(abi.encodePacked(msg.sender, _name, _startTime)));
+
+        // Create a Challenge struct with the provided parameters
         challenges[challengeId] = Challenge({
             challengeId: challengeId,
             name: _name,
@@ -34,10 +42,11 @@ contract ChallengeManagement {
             hardLevelTarget: _hardLevelTarget
         });
 
-        // Emit an event for challenge creation
+        // Emit an event to log the creation of a new challenge
         emit ChallengeCreated(challengeId, _name, _startTime, _endTime);
     }
 
+    // Event emitted when a new challenge is created
     event ChallengeCreated(
         uint256 indexed challengeId,
         string name,
